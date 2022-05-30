4dd1832e8a4202cfb54d01e4c0cdbb7ca8bb5fd5
double <82323135+double-sucks@users.noreply.github.com>
2022-05-30 21:59:42 +0300

    Add hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<script>
+import Canvas from "./Canvas.vue"
+
+export default {
+    components: {
+        Canvas
+    }
+}
+</script>
+
+
+<template>
+    <div class="hero">
+        <div class="hero-card">
+            <h1 class="hero-title">Hello, I am <b>double</b>.</h1>
+        </div>
+        <Canvas />
+    </div>
+</template>
+
+<style>
+</style>
