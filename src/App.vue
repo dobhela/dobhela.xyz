43f891d19c0d7eb03993ac7bfbef7462e862d145
double <82323135+double-sucks@users.noreply.github.com>
2022-05-30 22:00:00 +0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import Header from "./components/Header.vue"
+import Hero from "./components/Hero.vue"
+
+export default {
+    components: {
+        Header,
+        Hero
+    }
+}
+</script>
+
+<template>
+    <Header />
+    <main>
+        <Hero />
+    </main>
+</template>
+
+<style>
+    @import "./css/main.css"
+</style>
